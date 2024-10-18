cb4dc5f962e1e621b54e0eca242dc1b116750bc1
Rommel <giostrasser@gmail.com>
2024-10-18 00:27:53 -0300

    Updates project to final version.
    Updated repository with the final version of the project presented to the professor.

--- main.py
+++ main.py
@@ -1,28 +1,121 @@
-from appJar import gui
-
-#Eu queria um desafio maior já que sou bem familiar com Python,
-#Por isso tentei Integrar uma biblioteca gráfica no Projeto.
-
-#The main game steps are processed here, ((NEVER)) redirect the flow to another file.
 import utilities
 
-graphics = 0 #0 - binary, 1 - unicode, 2 - console art
+# Print Menu
+utilities.print_logo()
+print('\n' * 2)
 
-#Menu
-print("Pedra Papel Tesoura!")
+# Build the prompt for choosing unicode characters.
+unicodeExample = utilities.int_to_unicode(0) + utilities.int_to_unicode(1) + utilities.int_to_unicode(2)
+unicodeExample = "1 - Unicode(" + unicodeExample + ")"
 
-#Build the prompt for Unicode character.
-#It is done this way so that in the case of a character change it is automatically updated.
-unicodeExample = utilities.IntToUnicode((0))
-unicodeExample = unicodeExample.__add__(utilities.IntToUnicode((1)))
-unicodeExample = unicodeExample.__add__(utilities.IntToUnicode((2)))
-unicodeExample = "1 - Unicode(".__add__(unicodeExample).__add__(")")
+# Prompt user to input game settings
+gamemode = int(input("Entre o modo de jogo\n0 - Player Contra Player\n1 - Player Contra IA\n2 - IA vs IA\n"))
 
-#0 - PvP, 1 - PvE- 2 - EvE
-gamemode = int(input("Entre o modo de jogo \n0 - Player Contra Player \n1 - Player Contra IA \n2 - IA vs IA\n"))
-graphics = int(input("Entre o modo gráfico \n0 - Texto(0,1,2) \n".__add__(unicodeExample).__add__("\n2 - Gráficos 2D\n")))
+graphics = int(input("Entre o modo gráfico\n0 - Texto(0 - Pedra, 1 - Tesoura, 2 - Papel)\n" + unicodeExample + "\n"))
 
-choice1 = int(input("1:"))
-choice2 = int(input("2:"))
-print(utilities.IntToUnicode(choice1), " vs ", utilities.IntToUnicode(choice2))
-print(utilities.GetWinner(choice1, choice2))
+# Clamp gamemode
+gamemode = max(0, min(gamemode, 2))
+
+# Clamp graphics
+graphics = max(0, min(graphics, 1))
+
+# Feedback the user graphic mode selection
+if graphics == 0:
+    print("\nExecutando em modo Texto! :)")
+elif graphics == 1:
+    print("\nExecutando em modo Unicode! 👍")
+print('\n')
+
+# Initialize global choice variables
+choice1 = 0  # Player 1 choice
+choice2 = 0  # Player 2 choice
+
+# Score for each player
+victories1 = 0
+victories2 = 0
+execute = True
+
+# Main game loop!
+while execute:
+    # Handle Game Loop for each gamemode!
+    if gamemode == 0:  # PVP
+        utilities.print_battle_options()
+        choice1 = utilities.process_input(input("Primeiro Jogador:"))
+        utilities.clear_view()  # Hide first player's choice
+        utilities.print_battle_options()
+        choice2 = utilities.process_input(input("Segundo Jogador:"))
+    elif gamemode == 1:  # PVE
+        utilities.print_battle_options()
+        choice1 = utilities.process_input(input("Escolha do Jogador:"))
+        choice2 = utilities.make_ai_choice()
+        botChoiceDisplay = ""
+        if graphics == 0:
+            botChoice = utilities.int_to_text(choice1)
+        elif graphics == 1:
+            botChoice = utilities.int_to_unicode(choice2)
+        print("Escolha da IA:", botChoiceDisplay)  # Print Bot Choice
+    elif gamemode == 2:  # CPU vs CPU
+        utilities.print_battle_options()
+        choice1 = utilities.make_ai_choice()
+        choice2 = utilities.make_ai_choice()
+        botChoiceDisplay = ""
+        if graphics == 0:
+            botChoice = utilities.int_to_text(choice2)
+        elif graphics == 1:
+            botChoice = utilities.int_to_unicode(choice2)
+        print("Escolha da IA:", botChoiceDisplay)  # Print Bot Choice
+        utilities.clear_view()
+
+    # Process Match results
+    player1Print = ""
+    player2Print = ""
+    vsPrint = ""
+
+    # Process Graphics Choice
+    if graphics == 0:
+        player1Print = utilities.int_to_text(choice1)
+        player2Print = utilities.int_to_text(choice2)
+        vsPrint = "vs"
+    elif graphics == 1:
+        player1Print = utilities.int_to_unicode(choice1)
+        player2Print = utilities.int_to_unicode(choice2)
+        vsPrint = "vs"
+
+    # Print Result
+    print(player1Print, vsPrint, player2Print)
+    winner = utilities.get_winner(choice1, choice2)
+    utilities.print_outcome(winner, gamemode)
+
+    # Set player names based on gamemode
+    player1Name = "Jogador 1"
+    player2Name = "Jogador 2"
+
+    if gamemode == 1:
+        player1Name = "Jogador"
+        player2Name = "CPU"
+    if gamemode == 2:
+        player1Name = "CPU 1"
+        player2Name = "CPU 2"
+
+    # Print Final Score
+    if winner == 0:
+        print(player1Name, ">", victories1, "Vitórias (-)")
+        print(player2Name, ">", victories2, "Vitórias (-)")
+    elif winner == 1:
+        victories1 += 1
+        print(player1Name, ">", victories1, "Vitórias (+1)")
+        print(player2Name, ">", victories2, "Vitórias")
+    elif winner == 2:
+        victories2 += 1
+        print(player1Name, ">", victories1, "Vitórias")
+        print(player2Name, ">", victories2, "Vitórias (+1)")
+
+    # End of loop / Check if User wishes to continue
+    print('\n' * 2)
+    prompt = utilities.process_input_for_loop(input("Gostaria de Continuar ou Sair?\n0 - Continuar/C\n1 - Sair/S\n"))
+    execute = prompt == 0  # Continue game with any other prompt other than 1 or 'continuar'
+
+if not execute:
+    utilities.print_final_results(gamemode, victories1, victories2)  # Print final results and credits.
+else:
+    utilities.clear_view()
--- utilities.py
+++ utilities.py
@@ -1,38 +1,146 @@
-#Use this class to define functions!
+# RUN main.py NOT THIS FILE! THIS IS ONLY A HEADER!
+# Only use this class to define functions!
 
-#Converts Ints to Unicode graphics, wow!
-def IntToUnicode(_int):
+import datetime
+import random
+
+# Converts Ints to Unicode graphics, wow!
+def int_to_unicode(_int):
+    # Converts the choice int to unicode
     if _int == 0:  # Rock
         return "✊"
-    if _int == 1:  # Scissors:
-        return "✌"
-    if _int == 2:  # Paper:
+    if _int == 1:  # Paper
         return "🤚"
+    if _int == 2:  # Scissors
+        return "✌"
     return "ERRO"
 
-#Replaces the given section of the console with a hidden version of the choices from each player...
-def HideChoice(_text):
-    temp = str.replace(_text, "✊", "*")
-    temp = str.replace(_text, "✌", "*")
-    temp = str.replace(_text, "🤚", "*")
+def int_to_text(_int):
+    # Converts the choice int to text
+    if _int == 0:  # Rock
+        return "Pedra"  # GetASCIIRock() <-- I planned to use ASCII art for text mode but regular text works better.
+    if _int == 1:  # Paper
+        return "Papel"  # GetASCIIPaper()
+    if _int == 2:  # Scissors
+        return "Tesoura"  # GetASCIIScissors()
+    return "ERRO"
+
+victory_matrix = [[0,2,2], [1,0,2], [1,1,0]]
+#New and Improved Get Winner Check!
+def get_winner(_player1, _player2): #0 - Draw, 1 - choice 1 wins, 2 - choice 2 wins.
+    return victory_matrix[_player1][ _player2]
+
+def print_outcome(_outcome, _mode):
+    # Displays the state of the game at the end of a match
+    player1 = "Jogador 1"
+    player2 = "Jogador 2"
+
+    # Convert name display to fit gamemode
+    if _mode == 1:
+        player1 = "Jogador"
+        player2 = "CPU"
+    if _mode == 2:
+        player1 = "CPU 1"
+        player2 = "CPU 2"
+
+    if _outcome == 0:
+        print("Empate!")
+    elif _outcome == 1:
+        print(player1, "vence!")
+    elif _outcome == 2:
+        print(player2, "vence!")
+
+def process_input(_input):
+    # Converts input to int
+    temp = -1  # -1 means the input is invalid!
+
+    try:
+        temp = int(_input)  # If input is given as int
+        if temp not in range(0, 3):  # Check for an invalid choice
+            temp = -1  # Turn the result into an error
+    except:
+        tempText = _input.lower()  # If input is given as a string; also send it to lower case for ease of checking
+        if tempText == "pedra" or tempText == "rock" or tempText == "r":
+            temp = 0
+        elif tempText == "papel" or tempText == "paper" or tempText == "p":
+            temp = 1
+        elif tempText == "tesoura" or tempText == "scissors" or tempText == "s" or tempText == "t":
+            temp = 2
+
+    if temp == -1:  # In case of no valid string given!
+        print("Nenhum valor válido foi dado, o computador escolherá em seu lugar!")
+        temp = make_ai_choice()
+
     return temp
 
-#Returns the winner of a battle.
-def GetWinner(_choice1, _choice2): #0 - Draw, 1 - choice 1 wins, 2 - choice 2 wins.
-    #Edge Cases
-    if _choice1 == _choice2: return 0 #Draw
-    if _choice1 == 2 and _choice2 == 0: return 1 # paper vs rock
-    if _choice2 == 2 and _choice1 == 0: return 2 # paper vs rock
+def process_input_for_loop(_input):
+    # Converts input to int
+    temp = 0  # In the case of an invalid input, the game MUST GO ON!
 
-    #Most Cases
-    if _choice1 < _choice2: return 1
-    if _choice1 > _choice2: return 2
+    try:
+        temp = int(_input)  # If input is given as int
+    except:
+        tempText = _input.lower()  # If input is given as a string; also send it to lower case for ease of checking
+        if tempText == "continuar" or tempText == "c":
+            temp = 0
+        elif tempText == "sair" or tempText == "s":
+            temp = 1
 
-def OutComeToText(_outcome):
-    if _outcome == 0:
-        return "Empate!"
-    elif _outcome == 1:
-        return
+    return temp
 
+def make_ai_choice():
+    # Gets the bot's decision
+    random.seed = datetime.datetime.now()  # Change seed to decrease the chance of draws in CPU vs CPU
+    return random.randrange(3)
 
-#def PrintBattle(_choice1, _choice2):
+def print_battle_options():
+    # Displays all available options
+    print("#######Faça sua escolha######")
+    print("0 - Pedra/Rock/R")
+    print("1 - Papel/Paper/P")
+    print("2 - Tesoura/Scissors/T/S")
+
+def clear_view():
+    # Clears the console view
+    print('\n' * 100)
+
+def print_final_results(_mode, _victories1, _victories2):
+    # Prints the final result after the user wants to quit
+    player1 = "Jogador 1"
+    player2 = "Jogador 2"
+
+    # Convert name display to fit gamemode
+    if _mode == 1:
+        player1 = "Jogador"
+        player2 = "CPU"
+    if _mode == 2:
+        player1 = "CPU 1"
+        player2 = "CPU 2"
+
+    # Print Victory Table
+    print('#' * 50)
+    print(player1, "Vitórias totais:", _victories1)
+    print(player2, "Vitórias totais:", _victories2)
+    print("\n", end="")
+
+    # Decide and print the final Winner
+    print("Resultado final: ", end="")
+    if _victories1 == _victories2:
+        print(" Empate!")
+    elif _victories1 > _victories2:
+        print(player1, "Vence!")
+    else:
+        print(player2, "Vence!")
+
+    print("Obrigado por jogar!")
+    print("Desenvolvido por Giovanni Galarda Strasser. ☕")
+    print('#' * 50)
+
+# Graphical Prints for ASCII art
+def print_logo():
+    # Main Menu
+    print(
+        "| ) | \n|-' ,-. ,-| ;-. ,-: \n|"
+        "   |-' | | | | | \n' `-' `-' ' `-` \n"
+        "\n;-. . \n| ) | \n|-' ,-: ;-. ,-. | \n"
+        "| | | | | |-' | \n' `-` |-' `-' ' `-` o ")
